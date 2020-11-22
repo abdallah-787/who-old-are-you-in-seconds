4afb5b22ce414c98161eb946a2163d3aec85ba9c
abdallah-787 <74855333+abdallah-787@users.noreply.github.com>
2020-11-22 12:46:23 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,14 +2,17 @@ import time
 
 print("Hi!!!!!")
 time.sleep(1)
+#the name input
 name =input("what's your name??? ")
 time.sleep(1)
 print("Hello "+name)
 time.sleep(1)
 print("i will now calculate your old")
 time.sleep(1)
+#the age input
 age =  str(int(input("who old are you: ")))
 time.sleep(2)
+#not so important just for fun
 print("10%")
 time.sleep(1)
 print("20%")
@@ -24,6 +27,7 @@ time.sleep(1)
 print("well you are "+age+" old")
 print("hmmmmmm")
 time.sleep(1)
+#the calculate
 print("i think you are about "+str(int(age)*12*31)+" days")
 time.sleep(1)
 print(str(int(age)*12*31*24)+" hours")
